c729f9585aa63e9edd5618bea4005332b31f9deb
jerk <2931609377@qq.com>
2022-03-15 12:38:32 +0800

    first commit

---
+++ src/main/java/com/test/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.test.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
